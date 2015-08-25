23b565385dd73221117a4537cbb88800b054f987
Florian Mayer <Florian.Mayer@dpaw.wa.gov.au>
2015-08-25 00:40:48 +0000

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env/python
+from setuptools import setup
+
+setup(
+    name='ckanext-datawagovautheme',
+    version='0.1',
+    description='',
+    license='AGPL3',
+    author='data.wa.gov.au team',
+    author_email='florian.mayer@dpaw.wa.gov.au',
+    url='http://govhack2015.readthedocs.org/',
+    namespace_packages=['ckanext'],
+    packages=['ckanext.datawagovautheme'],
+    zip_safe=False,
+    entry_points = """
+        [ckan.plugins]
+        datawagovau_theme = ckanext.datawagovautheme.plugins:CustomTheme
+    """
+)
