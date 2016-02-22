9392d377c118c3f57f4336c810fbc80751c59afd
Yan Rudenko <yan.rudenko@linkdigital.com.au>
2016-02-22 17:25:56 +0200

    LANDXCDFB-52 / Implement Desktop version

--- ckanext/datawagovautheme/fanstatic/drop_down_menu.js
+++ ckanext/datawagovautheme/fanstatic/drop_down_menu.js
@@ -4,7 +4,6 @@ ckan.module('drop_down_menu', function ($, _) {
   return {
     initialize: function () {
     	$.proxyAll(this, /_on/);
-      	console.log("I've been initialized for element: ", this.el);
       	this.el.find('li a').each(function() {
       		if ($(this).attr('href') == location.pathname) {
       			$(this).parent().addClass('active');
@@ -13,13 +12,13 @@ ckan.module('drop_down_menu', function ($, _) {
       	this.el.find('li.active').parents('.nav.dropdown-menu').css({
       		"display": "block"
       	})
-      	//console.log(this.el.find('.nav:not(.dropdown-menu)').find('li'));
       	this.el.find('li.active').parents('li').addClass('children-active');
-      	//console.log(this.el.closest('ul.nav:not(.dropdown-menu)'));
-      	// this.el.find('li a').on('click', function(event) {
-      	// 	console.log($(this));
-      	// 	//event.preventDefault();
-      	// });
+      	if (this.el.find('li.children-active').length == 1) {
+			$('#content').css("padding-top", "121px");
+		} else {
+			console.log("li.children-active not found");
+			$('#content').css("padding-top", "0px");
+		}
       }
   };
 });
