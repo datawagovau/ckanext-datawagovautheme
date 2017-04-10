5bef2fa41b7b747fddf264404882c584a7ac85bf
Artem Bazykin <artem.bazykin@linkdigital.com.au>
2017-04-10 12:06:21 +0300

    SXLANDXZDF-40 / Update copyright statement to 2017

--- ckanext/datawagovautheme/plugins.py
+++ ckanext/datawagovautheme/plugins.py
@@ -1,10 +1,23 @@
 
-from ckan.plugins import toolkit, IConfigurer, SingletonPlugin, implements
+from ckan.plugins import (toolkit, IConfigurer, SingletonPlugin,
+                          ITemplateHelpers, implements)
+import datetime
+
+
+def get_current_year():
+    return datetime.datetime.today().year
+
 
 class CustomTheme(SingletonPlugin):
     implements(IConfigurer)
+    implements(ITemplateHelpers)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, "templates")
         toolkit.add_public_directory(config, "static")
         toolkit.add_resource('fanstatic', 'datawagovautheme')
+
+    def get_helpers(self):
+        return {
+            'get_current_year': get_current_year
+        }
