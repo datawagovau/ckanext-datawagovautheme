47d44d5e8e595795102c0066bfe691221adf4946
Mark Gregson <mark.gregson@linkdigital.com.au>
2017-05-18 21:13:21 +1000

    Update version to 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ckanext-datawagovautheme',
-    version='2.0.0',
+    version='2.0.1',
     description='',
     license='AGPL3',
     author='data.wa.gov.au team',
