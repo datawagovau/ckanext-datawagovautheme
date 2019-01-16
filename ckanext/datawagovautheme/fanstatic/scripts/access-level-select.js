eec0b2564350e3401c366f3928dccc078463be15
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2019-01-16 12:58:08 +0200

    LANDXCFCH-8 /  Access Restrictions Fields

---
+++ ckanext/datawagovautheme/fanstatic/scripts/access-level-select.js
@@ -0,0 +1,27 @@
+ckan.module('access-level-select', function($, _) {
+  'use strict';
+
+  return {
+    options: {
+      match: null,
+      target: null,
+      extendTo: null
+    },
+    initialize: function() {
+      $.proxyAll(this, /_on/);
+      this.el.on('change', this._onChange);
+
+      this._onChange();
+    },
+    _onChange: function() {
+      var val = this.el.val();
+      var target = $(this.options.target);
+      if (this.options.extendTo) {
+        target = target.closest(this.options.extendTo);
+      }
+      val.match(this.options.match)
+        ? (target.find('input').attr('disabled', true), target.hide())
+        : (target.find('input').removeAttr('disabled'), target.show());
+    }
+  };
+});
