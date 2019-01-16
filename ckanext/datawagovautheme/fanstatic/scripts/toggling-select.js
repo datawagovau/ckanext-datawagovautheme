b496763c48feeaac00f4b09969493ca0cd7bff54
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2019-01-16 14:34:52 +0200

    LANDXCFCH-9 / license fields

--- ckanext/datawagovautheme/fanstatic/scripts/toggling-select.js
+++ ckanext/datawagovautheme/fanstatic/scripts/toggling-select.js
@@ -1,11 +1,12 @@
-ckan.module('access-level-select', function($, _) {
+ckan.module('toggling-select', function($, _) {
   'use strict';
 
   return {
     options: {
       match: null,
       target: null,
-      extendTo: null
+      extendTo: null,
+      inverse: false
     },
     initialize: function() {
       $.proxyAll(this, /_on/);
@@ -19,7 +20,7 @@ ckan.module('access-level-select', function($, _) {
       if (this.options.extendTo) {
         target = target.closest(this.options.extendTo);
       }
-      val.match(this.options.match)
+      !!val.match(this.options.match) ^ this.options.inverse
         ? (target.find('input').attr('disabled', true), target.hide())
         : (target.find('input').removeAttr('disabled'), target.show());
     }
