b4587545825dd80e46deef7ead2f1ea6e1de8069
Yan <yan@rudenko.com>
2019-01-17 15:11:25 +1100

    LANDXCFCH-7 / updating resource access level

---
+++ ckanext/datawagovautheme/fanstatic/scripts/resource-warning.js
@@ -0,0 +1,28 @@
+ckan.module('resource-warning', function($, _) {
+  'use strict';
+
+  return {
+    options: {
+      match: null,
+      target: null,
+      inverse: false
+    },
+    initialize: function() {
+      $.proxyAll(this, /_on/);
+      this.el.on('change', this._onChange);
+
+      this._onChange();
+    },
+    _onChange: function() {
+      var options = (this.options.match).split(',');
+      var val = this.el.val();
+      var target = $(this.options.target);
+
+      jQuery.inArray(val, options) > -1 ? (
+          target.attr('disabled', true), target.hide()
+        ):(
+          target.removeAttr('disabled'), target.show()
+        );
+    }
+  };
+});
